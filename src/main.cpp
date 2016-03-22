0bcc1741de95835e40dab733f93b1925d0f19e03
Rajmund Szymanski <rajszym@o2.pl>
2016-03-22 20:00:07 +0100

    minor update, add c++ example

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <stm32f4_discovery.h>
+#include <os.h>
+
+void proc( unsigned &led, unsigned timePoint )
+{
+	for (;;)
+	{
+		ThisTask::sleepUntil(timePoint);
+		timePoint += SEC/2;
+		led++;
+	}
+}
+
+auto led = Led();
+auto grn = GreenLed();
+
+auto t1 = startTask([]{ proc(led[0], 500*MSEC); });
+auto t2 = startTask([]{ proc(led[1], 625*MSEC); });
+auto t3 = startTask([]{ proc(led[2], 750*MSEC); });
+auto t4 = startTask([]{ proc(led[3], 875*MSEC); });
+auto t5 = startTask([]{ proc(grn,   1000*MSEC); });
+
+int main()
+{
+    sys_init();
+
+	ThisTask::stop();
+}
