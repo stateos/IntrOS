f6a526436b73690e1493a7da0b1ef4bf199a84ee
Rajmund Szymanski <rajszym@o2.pl>
2017-01-08 12:41:20 +0100

    rebuilded internal preprocessor definitions

--- IntrOS/interface/inc/os_mem.h
+++ IntrOS/interface/inc/os_mem.h
@@ -2,7 +2,7 @@
 
     @file    IntrOS: os_mem.h
     @author  Rajmund Szymanski
-    @date    07.01.2017
+    @date    08.01.2017
     @brief   This file contains definitions for IntrOS.
 
  ******************************************************************************
@@ -72,7 +72,7 @@ struct __mem
  *                                                                                                                    *
  **********************************************************************************************************************/
 
-#define               _MEM_INIT( limit, size, data ) { 0, limit, MSIZE(size), data }
+#define               _MEM_INIT( _limit, _size, _data ) { 0, _limit, MSIZE(_size), _data }
 
 /**********************************************************************************************************************
  *                                                                                                                    *
@@ -96,6 +96,25 @@ struct __mem
 
 /**********************************************************************************************************************
  *                                                                                                                    *
+ * Name              : MEM_DEF                                                                                        *
+ *                                                                                                                    *
+ * Description       : define and initilize complete memory pool area                                                 *
+ *                                                                                                                    *
+ * Parameters                                                                                                         *
+ *   mem             : name of a memory pool area                                                                     *
+ *   limit           : size of a buffer (max number of objects)                                                       *
+ *   size            : size of memory object (in bytes)                                                               *
+ *                                                                                                                    *
+ * Note              : for internal use                                                                               *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
+
+#define                MEM_DEF( _mem, _limit, _size )                                   \
+                       struct { mem_t obj; void *data[_limit*(1+MSIZE(_size))]; } _mem = \
+                       { _MEM_INIT( _limit, _size, _mem.data ), { 0 } }
+
+/**********************************************************************************************************************
+ *                                                                                                                    *
  * Name              : OS_MEM                                                                                         *
  *                                                                                                                    *
  * Description       : define and initilize a memory pool object                                                      *
@@ -107,10 +126,9 @@ struct __mem
  *                                                                                                                    *
  **********************************************************************************************************************/
 
-#define             OS_MEM( mem, limit, size )                                \
-                       void*mem##__buf[limit*(1+MSIZE(size))];                 \
-                       mem_t mem##__mem = _MEM_INIT( limit, size, mem##__buf ); \
-                       mem_id mem = & mem##__mem
+#define             OS_MEM( mem, limit, size )            \
+                       MEM_DEF( mem##__mem, limit, size ); \
+                       mem_id mem = & mem##__mem.obj
 
 /**********************************************************************************************************************
  *                                                                                                                    *
@@ -125,10 +143,9 @@ struct __mem
  *                                                                                                                    *
  **********************************************************************************************************************/
 
-#define         static_MEM( mem, limit, size )                                \
-                static void*mem##__buf[limit*(1+MSIZE(size))];                 \
-                static mem_t mem##__mem = _MEM_INIT( limit, size, mem##__buf ); \
-                static mem_id mem = & mem##__mem
+#define         static_MEM( mem, limit, size )            \
+                static MEM_DEF( mem##__mem, limit, size ); \
+                static mem_id mem = & mem##__mem.obj
 
 /**********************************************************************************************************************
  *                                                                                                                    *
