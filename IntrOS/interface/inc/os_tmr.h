99a7929fa933b7e9a6e86904fb4c78e4dc7d2c1d
Rajmund Szymanski <rajszym@o2.pl>
2017-04-02 19:34:57 +0200

    updated OS_TMR / static_TMR macros

--- IntrOS/interface/inc/os_tmr.h
+++ IntrOS/interface/inc/os_tmr.h
@@ -2,7 +2,7 @@
 
     @file    IntrOS: os_tmr.h
     @author  Rajmund Szymanski
-    @date    30.03.2017
+    @date    02.04.2017
     @brief   This file contains definitions for IntrOS.
 
  ******************************************************************************
@@ -85,8 +85,7 @@ struct __tmr
  *                                                                                                                    *
  **********************************************************************************************************************/
 
-#define             OS_TMR( tmr, state )                    \
-                       void state( void );                   \
+#define             OS_TMR( tmr, state )                     \
                        tmr_t tmr##__tmr = _TMR_INIT( state ); \
                        tmr_id tmr = & tmr##__tmr
 
@@ -103,8 +102,7 @@ struct __tmr
  *                                                                                                                    *
  **********************************************************************************************************************/
 
-#define         static_TMR( tmr, state )                    \
-                static void state( void );                   \
+#define         static_TMR( tmr, state )                     \
                 static tmr_t tmr##__tmr = _TMR_INIT( state ); \
                 static tmr_id tmr = & tmr##__tmr
 
