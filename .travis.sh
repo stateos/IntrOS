ee78c37e5576ecee7718ce9ca87deca0c143a889
rajszym <rajszym@o2.pl>
2017-10-10 09:37:29 +0200

    travis-ci

--- .travis.sh
+++ .travis.sh
@@ -7,21 +7,20 @@ if [ ! -d examples ]; then
 	exit 0
 fi
 
-rm -f src/main.c
 rm -f src/main.cpp
-
 for file in examples/*.c\ *
 do
+	echo $file
 	cp "$file" src/main.c
 	touch src/main.c
 	make all GNUCC=arm-none-eabi- -f makefile.gnucc
-	rm -f src/main.c
 done
 
+rm -f src/main.c
 for file in examples/*.cpp*
 do
+	echo $file
 	cp "$file" src/main.cpp
 	touch src/main.cpp
 	make all GNUCC=arm-none-eabi- -f makefile.gnucc
-	rm -f src/main.cpp
 done
