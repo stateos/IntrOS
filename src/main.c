4218d4c5f2ba93fee5dc135ec56eb6b6566b2b77
Rajmund Szymanski <rajszym@o2.pl>
2017-11-04 17:17:50 +0100

    added startup files for cosmic compiler

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <stm32f4_discovery.h>
+#include <os.h>
+
+OS_SEM(sem, 0);
+
+OS_TSK_DEF(sla)
+{
+	sem_wait(sem);
+	LED_Tick();
+}
+
+OS_TSK_DEF(mas)
+{
+	tsk_delay(SEC);
+	sem_give(sem);
+}
+
+int main()
+{
+	LED_Init();
+
+	tsk_start(sla);
+	tsk_start(mas);
+	tsk_sleep();
+}
