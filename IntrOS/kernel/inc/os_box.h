fa3c4595e7028cb9744968143c402dfc2e6924b0
rajszym <rajszym@o2.pl>
2018-04-16 08:12:18 +0200

    updated mail box queue

--- IntrOS/kernel/inc/os_box.h
+++ IntrOS/kernel/inc/os_box.h
@@ -2,7 +2,7 @@
 
     @file    IntrOS: os_box.h
     @author  Rajmund Szymanski
-    @date    15.04.2018
+    @date    16.04.2018
     @brief   This file contains definitions for IntrOS.
 
  ******************************************************************************
@@ -74,7 +74,7 @@ struct __box
  *
  ******************************************************************************/
 
-#define               _BOX_INIT( _limit, _data, _size ) { 0, _limit, 0, 0, _data, _size }
+#define               _BOX_INIT( _limit, _data, _size ) { 0, _limit * _size, 0, 0, _data, _size }
 
 /******************************************************************************
  *
