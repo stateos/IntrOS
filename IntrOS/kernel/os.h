785b9659d2d694b0382cf6a99cdcca4e50a04996
Rajmund Szymański <rajszym@users.noreply.github.com>
2018-10-23 07:52:39 +0200

    updated os version

--- IntrOS/kernel/os.h
+++ IntrOS/kernel/os.h
@@ -2,7 +2,7 @@
 
     @file    IntrOS: os.h
     @author  Rajmund Szymanski
-    @date    21.09.2018
+    @date    23.10.2018
     @brief   This file contains definitions for IntrOS.
 
  ******************************************************************************
@@ -32,7 +32,7 @@
 #ifndef __INTROS
 
 #define __INTROS_MAJOR        4
-#define __INTROS_MINOR        3
+#define __INTROS_MINOR        4
 #define __INTROS_BUILD        0
 
 #define __INTROS        ((((__INTROS_MAJOR)&0xFFUL)<<24)|(((__INTROS_MINOR)&0xFFUL)<<16)|((__INTROS_BUILD)&0xFFFFUL))
