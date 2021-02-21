a757f2bd3c6077a456edd9354bd27130ee9ff316
rajszym <rajszym@o2.pl>
2021-02-21 19:51:38 +0100

    minor updates

--- IntrOS/port/.cortexm/common/osmpu.h
+++ IntrOS/port/.cortexm/common/osmpu.h
@@ -1,9 +1,9 @@
 /******************************************************************************
 
-    @file    StateOS: osmpu.h
+    @file    IntrOS: osmpu.h
     @author  Rajmund Szymanski
-    @date    01.12.2020
-    @brief   This file defines set of memory protection unit functions for StateOS.
+    @date    21.02.2021
+    @brief   This file defines set of memory protection unit functions for IntrOS.
 
  ******************************************************************************
 
@@ -29,8 +29,8 @@
 
  ******************************************************************************/
 
-#ifndef __STATEOSMPU_H
-#define __STATEOSMPU_H
+#ifndef __INTROSMPU_H
+#define __INTROSMPU_H
 
 #include "osport.h"
 
@@ -40,15 +40,15 @@
 #define OS_GUARD_SIZE    32 /* ARM_MPU_REGION_SIZE_32B */
 #endif
 
-#if defined (OS_GUARD_SIZE) && ((OS_GUARD_SIZE) >= 32) && ((OS_GUARD_SIZE) == ((OS_GUARD_SIZE) & ~((OS_GUARD_SIZE) - 1)))
-
 #ifndef __MPU_USED
+#if defined (OS_GUARD_SIZE) && ((OS_GUARD_SIZE) >= 32) && ((OS_GUARD_SIZE) == ((OS_GUARD_SIZE) & ~((OS_GUARD_SIZE) - 1)))
 #define __MPU_USED        1
+#endif
 #elif   __MPU_USED
 #error  __MPU_USED is an internal os definition!
 #endif//__MPU_USED
 
-#if     __MPU_USED == 1
+#if defined (__MPU_USED) && (__MPU_USED == 1)
 
 #ifdef __cplusplus
 extern "C" {
@@ -195,6 +195,5 @@ void port_mpu_stackUpdate( const void *stk )
 /* -------------------------------------------------------------------------- */
 
 #endif//__MPU_USED
-#endif//OS_GUARD_SIZE
 #endif//__MPU_PRESENT
-#endif//__STATEOSMPU_H
+#endif//__INTROSMPU_H
