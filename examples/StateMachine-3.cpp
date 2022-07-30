d5b15c943cb46cd3f7010affa2d6b74999536536
rajszym <rajszym@o2.pl>
2022-07-30 11:07:20 +0200

    updated examples

--- examples/StateMachine-3.cpp
+++ examples/StateMachine-3.cpp
@@ -18,20 +18,15 @@ auto led        = device::Led();
 auto dispatcher = intros::Task(nullptr);
 auto StateOff   = intros::State();
 auto StateOn    = intros::State();
-auto blinker    = intros::StateMachineT<10>();
-
-std::vector<intros::Action> tab =
-{
-	{ StateOff, EventInit,   []( hsm_t *, unsigned ){ led = 0; } },
+auto blinker    = intros::StateMachineT<10>
+{{
 	{ StateOff, EventSwitch, StateOn },
 	{ StateOn,  EventSwitch, StateOff },
 	{ StateOn,  EventTick,   []( hsm_t *, unsigned ){ led.tick(); } },
-};
+}};
 
 int main()
 {
-	for (auto& a: tab) a.link();
-
 	blinker.start(dispatcher, StateOff);
 	blinker.send(EventSwitch);
 	for (;;)
